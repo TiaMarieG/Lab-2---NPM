d6f7488251240bec4552eaae82e15ad9d2fde8b9
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-01-08 17:49:04 -0800

    Testing installation of packages and creating scripts

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const array = [1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 8, 8, 8, 9, 10];
 
 // Using a function from lodash library to get unique numbers from the array
 const uniqueNums = lodash.uniq(array);
+console.log(chalk.red(array));
 console.log(chalk.yellow(uniqueNums));
 
 console.log(chalk.blueBright('Hello, with chalk!'));
